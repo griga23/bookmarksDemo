a60709ecc279a6739496422e6e64f71863610304
griga23 <spasitel2@gmail.com>
2021-03-02 14:12:45 +0100

    small refactor

--- BookmarksProducer/src/main/java/jans/BookmarksProducerApplication.java
+++ BookmarksProducer/src/main/java/jans/BookmarksProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookmarkClientKafkaApplication {
+public class BookmarksProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmarkClientKafkaApplication.class, args);
+		SpringApplication.run(BookmarksProducerApplication.class, args);
 	}
 
 }
